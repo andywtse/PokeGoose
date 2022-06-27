0e43f91225ed8cdeb4027a8afe763e4028ba8e65
andywtse <atse4995@gmail.com>
2022-06-27 16:01:00 -0400

    Adds functionality in the views

--- controllers/community.js
+++ controllers/community.js
@@ -1,11 +1,33 @@
 import { Team } from "../models/team.js";
 
-function index(req,res) {
-  
+function index(req, res) {
+  Team.find({})
+    .then(teams => {
+      res.render('community/index', {
+        teams,
+        title: "Community",
+        pokemons: teams.pokemons
+      });
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    });
 };
 
-function show(req,res) {
-
+function show(req, res) {
+  Team.findById(req.params.id)
+    .then(team => {
+      res.render(`teams/${req.params.id}`, {
+        title: team.title,
+        team,
+        pokemons: team.pokemons
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    });
 };
 
 
