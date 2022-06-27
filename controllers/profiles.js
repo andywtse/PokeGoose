993af2a8c6c8e0d87d6c33f71bd75c3f684a227c
andywtse <atse4995@gmail.com>
2022-06-27 13:54:31 -0400

    Adds skeleton of controllers

---
+++ controllers/profiles.js
@@ -0,0 +1,24 @@
+import { Profile } from "../models/profile.js";
+
+function index(req,res) {
+  
+};
+
+function create(req,res) {
+
+};
+
+function show(req,res) {
+
+};
+
+function update(req,res) {
+
+};
+
+export {
+  index,
+  create,
+  show,
+  update,
+}
---
+++ controllers/teams.js
@@ -0,0 +1,39 @@
+import { Team } from "../models/team.js";
+
+function index(req,res) {
+  
+};
+
+function newTeam(req,res) {
+
+};
+
+function create(req,res) {
+
+};
+
+function show(req,res) {
+
+};
+
+function edit(req,res) {
+
+};
+
+function update(req,res) {
+
+};
+
+function deleteTeam(req,res) {
+
+};
+
+export {
+  index,
+  newTeam as new,
+  create,
+  show,
+  edit,
+  update,
+  deleteTeam as delete
+}
